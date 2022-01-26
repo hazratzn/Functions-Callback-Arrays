38c2390088142929df032c213f400cf0a8417fc8
hazratzn <hazratzn@code.edu.az>
2022-01-26 11:58:46 -0800

    Callback Arrays

---
+++ Func,Array/assets/js/script.js
@@ -0,0 +1,137 @@
+"use strict";
+
+/*function getSum(n) {
+  let sum = 0;
+
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(getSum(3)); */
+
+/*function getSum(n) {
+  if (n == 0) {
+    return n;
+  } else {
+    return getSum(n - 1) + n;
+  }
+}
+console.log(getSum(3)); */
+
+
+//let arr = [1,2,3,4,5]
+
+/*function getArrayItems(m,n,...list){
+    let sum = n + m;
+    for (const item of list) {
+        sum += item;
+    }
+    return sum
+}
+getArrayItems(1,2,3,4,5,6,7,8,9)
+
+// function isEven(n){
+//     return n%2 == 0;
+// }
+
+// function isOdd(n){
+
+//     return n % 2 != 0
+// }
+
+
+// function isOverThree(n){
+//     return n > 3
+// }
+
+
+// let isOdd = function(n){
+//     return n % 2 != 0
+// }
+
+// let isOverThree = function(n){
+//     return n > 3
+// }
+
+
+// function getSumByContiditions(arr,callback){
+//     let result = 0;
+//     for (const item of arr) {
+
+//         if(callback(item)){
+//             result += item;
+//         }
+//     }
+
+//     return result;
+// }
+
+
+// console.log(getSumByContiditions(array,function(n){
+//     return n%2 == 0;
+// }))
+
+
+// console.log(getSumByContiditions(array,n => n>2))
+// console.log(getSumByContiditions(array,isOdd))
+// console.log(getSumByContiditions(array,isOverThree))
+
+//let array = [1, 2, 3, 4, 6, 8];
+
+
+// function createMap(arr,func){
+
+//     let result = [];
+
+//     for (const item of arr) {
+
+//         result.push(func(item))
+//     }
+//     return result;
+// }
+
+// function multiplyTwo(n){
+//     return n * 2
+// }
+
+
+
+
+// console.log(createMap(array,n => n*2));
+
+// let result = array.map(m => m);
+
+// console.log(result);
+
+// array[0] = 33;
+// console.log(array)
+// console.log(result);
+
+
+
+// function test(ar){
+//     return ar;
+// }
+
+// console.log(array.map(m => m *2));
+
+
+//  array.forEach((n,i) => {
+
+//     console.log(n + " " +i);
+
+// });
+
+
+
+
+
+// let result = array.map((n)=>{
+//     return n ++ ;
+// });
+// console.log(result);
+
+*/
